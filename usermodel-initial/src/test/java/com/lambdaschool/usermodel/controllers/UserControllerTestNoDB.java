d704ca253bfe9a4df62de0ee9b919b810642e4f2
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-03-25 02:55:08 -0400

    Running into some issues with tests getting access denied error. Submitting.

---
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTestNoDB.java
@@ -0,0 +1,262 @@
+package com.lambdaschool.usermodel.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.usermodel.UserModelApplicationTesting;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.services.UserService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.web.context.WebApplicationContext;
+import static org.mockito.ArgumentMatchers.any;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+classes = UserModelApplicationTesting.class,
+properties = {"command.line.runner.enabled=false"})
+@AutoConfigureMockMvc
+public class UserControllerTestNoDB {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private final List<User> userList = new ArrayList<>();
+
+    @Before
+    public void setUp() throws Exception
+    {
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
+
+        r1.setRoleid(1);
+        r2.setRoleid(2);
+        r3.setRoleid(3);
+
+        // admin, data, user
+        User u1 = new User("admin",
+                "password",
+                "admin@lambdaschool.local");
+        u1.setUserid(10);
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r1));
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r2));
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r3));
+        u1.getUseremails()
+                .add(new Useremail(u1,
+                        "admin@email.local"));
+        u1.getUseremails().get(0).setUseremailid(11);
+
+        userList.add(u1);
+//        userService.save(u1);
+
+        // data, user
+        User u2 = new User("cinnamon",
+                "1234567",
+                "cinnamon@lambdaschool.local");
+        u2.setUserid(20);
+        u2.getRoles()
+                .add(new UserRoles(u2,
+                        r2));
+        u2.getRoles()
+                .add(new UserRoles(u2,
+                        r3));
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "cinnamon@mymail.local"));
+        u2.getUseremails().get(0).setUseremailid(21);
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "hops@mymail.local"));
+        u2.getUseremails().get(1).setUseremailid(22);
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "bunny@email.local"));
+        u2.getUseremails().get(2).setUseremailid(23);
+
+        userList.add(u2);
+//        userService.save(u2);
+
+
+        // user
+        User u3 = new User("barnbarn",
+                "ILuvM4th!",
+                "barnbarn@lambdaschool.local");
+        u3.setUserid(30);
+        u3.getRoles()
+                .add(new UserRoles(u3,
+                        r2));
+        u3.getUseremails()
+                .add(new Useremail(u3,
+                        "barnbarn@email.local"));
+        u3.getUseremails().get(0).setUseremailid(31);
+
+        userList.add(u3);
+
+        User u4 = new User("puttat",
+                "password",
+                "puttat@school.lambda");
+        u4.setUserid(40);
+        u4.getRoles()
+                .add(new UserRoles(u4,
+                        r2));
+        userList.add(u4);
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void listAllUsers() throws Exception{
+        String apiURL = "/users/users";
+        Mockito.when(userService.findAll())
+                .thenReturn(userList);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(apiURL)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        String reqString = result.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String listString = mapper.writeValueAsString(userList);
+
+        assertEquals(listString, reqString);
+    }
+
+    @Test
+    public void getUserById() throws Exception{
+        String apiUrl = "/users/user/10";
+        Mockito.when(userService.findUserById(10))
+                .thenReturn(userList.get(0));
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb)
+                .andReturn();
+        String tr = r.getResponse()
+                .getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList.get(0));
+
+        System.out.println(tr);
+        assertEquals(er,
+                tr);
+    }
+
+    @Test
+    public void getUserByName() throws Exception{
+        String apiUrl = "/user/name/admin";
+
+        Mockito.when(userService.findByName("admin")).thenReturn(userList.get(0));
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb)
+                .andReturn();
+        String tr = r.getResponse()
+                .getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList.get(0));
+
+        System.out.println("Expect: " + er);
+        System.out.println("Actual: " + tr);
+
+        Assert.assertEquals("Rest API Returns List",
+                er,
+                tr);
+    }
+
+    @Test
+    public void getUserLikeName() throws Exception{
+        String apiUrl = "/user/user/name/like/admin";
+
+        Mockito.when(userService.findByNameContaining("admin"))
+                .thenReturn(userList);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb)
+                .andReturn();
+        String tr = r.getResponse()
+                .getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+
+        System.out.println("Expect: " + er);
+        System.out.println("Actual: " + tr);
+
+        Assert.assertEquals("Rest API Returns List",
+                er,
+                tr);
+    }
+
+    @Test
+    public void addNewUser() {
+    }
+
+    @Test
+    public void updateFullUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void deleteUserById() throws Exception{
+        String apiUrl = "/user/user/{id}";
+
+        RequestBuilder rb = MockMvcRequestBuilders.delete(apiUrl,
+                "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(rb)
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getCurrentUserInfo() {
+    }
+}
